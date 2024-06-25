b0f5649abe6a4f8eef51f4269aa35bc48fc422d8
Giancarlo Culcay <gieg5c@gmail.com>
2024-06-25 16:15:37 -0500

    removeConsole

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const myDate = Date()
+console.log("date: ", myDate)
+
 export default function Home() {
   return (
     <main
@@ -13,6 +16,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">pages/index.js</code>
         </p>
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
